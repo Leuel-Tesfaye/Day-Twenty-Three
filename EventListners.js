cddd24675ac850a9ba8daa4072e332117ca9aac2
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2023-07-06 11:40:10 +0300

    event commit

---
+++ EventListners.js
@@ -0,0 +1,8 @@
+/** Event listeners : common html events onClick, onChange, onMouseHover, onMouseOut, onKeyDown, onKeyUp, onLoad.
+ * 
+ */
+
+selectedElement.addEventListeners ('eventListener' , e=>{
+    //   // the activity you want to occur after the event will be in here
+
+})
