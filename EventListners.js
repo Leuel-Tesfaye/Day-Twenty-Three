30851f8ee1863a4f4351d0f476ba634e772db205
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2023-07-06 16:44:39 +0300

    click and dbl click event listeners commit

--- EventListners.js
+++ EventListners.js
@@ -2,7 +2,30 @@
  * 
  */
 
-selectedElement.addEventListeners ('eventListener' , e=>{
-    //   // the activity you want to occur after the event will be in here
+// selectedElement.addEventListeners ('eventListener' , e=>{
+//     //   // the activity you want to occur after the event will be in here
 
+// })
+
+// click : before we listen an event we select an element from the html 
+
+const button = document.querySelector('button')
+
+button.addEventListener('click', e =>{
+    button.textContent = 'Clicked'
 })
+
+// An event can be also attached directly to the HTML element as inline script.
+
+const button1 = document.querySelector('.submit')
+const clickMe = () => {
+    alert('Message submitted ')
+}
+
+//  double click 
+
+const download = document.querySelector('#double');
+
+download.addEventListener('dblclick', e => {
+    console.log('Downloaded', e);
+});
