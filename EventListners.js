895b678e4931596d446c878601829b283b357ed9
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2023-07-07 08:31:58 +0300

    event css file and input commit

--- EventListners.js
+++ EventListners.js
@@ -29,3 +29,34 @@ const download = document.querySelector('#double');
 download.addEventListener('dblclick', e => {
     console.log('Downloaded', e);
 });
+
+// mouse Enter , mouseHover etc are the same as the above just change the event listener to mouse enter or hover
+
+
+// Getting value from an input element  
+// we usually fill forms and accept data. form fields are created using input HTML 
+//Let us build a small application which allow us to calculate body mas index of a person using two input fields, one button and one p tag.
+
+const mass = document.querySelector('#mass')
+const height = document.querySelector('#height')
+const calculate = document.querySelector('.calculate')
+
+let bmi 
+calculate.addEventListener('click', e =>{
+    bmi = mass.value / height.value ** 2
+    alert(`your bmi is ${bmi.toFixed(2)}`)
+    console.log(bmi)
+})
+
+// exercising the above example with adding two numbers 
+
+const numOne = document.querySelector('#num1')
+const numTwo = document.querySelector('#num2')
+const add = document.querySelector('.sum')
+
+let sum 
+add.addEventListener('click', e =>{
+    sum = numOne.value + numTwo.value
+    alert(`The sum of the numbers is: ${sum}`)
+    console.log(sum)
+})
